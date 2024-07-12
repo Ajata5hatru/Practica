07cc82a8820b2e6657da61cb4e0139b711617f5e
Silvia Solari <168376402+Ajata5hatru@users.noreply.github.com>
2024-07-12 19:48:48 -0300

    A bit more code, nothing spetial

--- CodeCamp/Numeral Converter/javascript.js
+++ CodeCamp/Numeral Converter/javascript.js
@@ -1,3 +1,15 @@
-const input = documet.getElementByID("number");
+const userInput = documet.getElementByID("number");
 const convertBtn = document.getElementById("convert-btn");
-const output = document.getElementById("output");
+const outputDiv = document.getElementById("output");
+
+convertBtn.addEventListener("click", (e) => {
+  const inputWarning = () => {
+    if (userInput.value.length === 0) {
+      outputDiv.innerText = "Please enter a valid number.";
+      outputDiv.style.display = "block";
+      return;
+    } else {
+      outputDiv.innerText = "Please enter a number between 0 and 6333";
+    }
+  };
+});
